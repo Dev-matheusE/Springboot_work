4ae55f82ebbd2a1bc04aa7bf5e678bc73e434777
Dev-matheusE <matheusesdras95@gmail.com>
2024-11-10 21:39:21 -0300

    Product

--- src/main/java/com/matheus/course/repositories/CategoryRepository.java
+++ src/main/java/com/matheus/course/repositories/CategoryRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.matheus.course.entities.Category;
 
-import com.matheus.course.entities.Order;
 
 public interface  CategoryRepository extends JpaRepository<Category,Long> {
 
