6d4e828d8fbd07e7c343b7a1e0c8148465ddc9dc
Dev-matheusE <matheusesdras95@gmail.com>
2024-11-24 15:33:58 -0300

    Exception handling - update

--- src/main/java/com/matheus/course/services/UserService.java
+++ src/main/java/com/matheus/course/services/UserService.java
@@ -13,6 +13,10 @@ import com.matheus.course.repositories.UserRepository;
 import com.matheus.course.services.exceptions.DatabaseException;
 import com.matheus.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
+
+
 @Service
 public class UserService {
 
@@ -43,9 +47,13 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		}catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
