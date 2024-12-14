5f06605fe19872fa9ec5f2b3aca88fb96f990818
Dev-matheusE <matheusesdras95@gmail.com>
2024-12-14 14:01:48 -0300

    StandardError fix

--- src/main/java/com/matheus/course/resources/exceptions/StandardError.java
+++ src/main/java/com/matheus/course/resources/exceptions/StandardError.java
@@ -16,7 +16,6 @@ public class StandardError implements Serializable {
 	private String path;
 	
 	public StandardError() {
-		
 	}
 
 	public StandardError(Instant timestamp, Integer status, String error, String message, String path) {
@@ -27,6 +26,48 @@ public class StandardError implements Serializable {
 		this.message = message;
 		this.path = path;
 	}
+
+	public Instant getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(Instant timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public String getError() {
+		return error;
+	}
+
+	public void setError(String error) {
+		this.error = error;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+	
+	
 	
 	
 	
